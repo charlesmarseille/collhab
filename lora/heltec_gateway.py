32ff94a41ef27a809a7d8783ae8b89793734b4f8
interface-graphique <guillaumedostie@icloud.com>
2022-03-18 20:28:51 -0400

    added rssi to message in heltec_gateway.py

--- lora/heltec_gateway.py
+++ lora/heltec_gateway.py
@@ -2,7 +2,7 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0',115200)
 
 while True:
-    mess = ser.readline()
+    mess = ser.readline().decode()
     print(mess)
     with open('heltec_gateway_output.txt', 'a+') as f:
-        f.write(mess.decode())
+        f.write(mess)
